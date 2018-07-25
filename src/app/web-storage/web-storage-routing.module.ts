377b32e3939db300fda5ba8e02687eef8bb4fd1d
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2018-07-25 09:20:57 +0300

    new line

--- src/app/web-storage/web-storage-routing.module.ts
+++ src/app/web-storage/web-storage-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { WebStorageComponent } from './web-storage/web-storage.component';
 
 const routes: Routes = [
