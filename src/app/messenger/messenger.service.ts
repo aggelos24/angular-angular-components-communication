8ee92eff156fa11efa8b11f2a011233a09797952
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2018-07-25 09:30:13 +0300

    spaces

--- src/app/messenger/messenger.service.ts
+++ src/app/messenger/messenger.service.ts
@@ -9,7 +9,7 @@ export class MessengerService {
 	private messageSource = new BehaviorSubject<Message>(null);		//create BehaviorSubject with null as initial value
 	message = this.messageSource.asObservable();				//this is the variable to which recipients subscribe
 	
-	setMessage(message: Message) {						//this is called by the sender with the message it wants to send					
+	setMessage(message: Message) {						//this is called by the sender with the message it wants to send
 		this.messageSource.next(message);				//message is emitted
 	}
 }
