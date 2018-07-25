6ad577d31a6bc06ac9a19e1675350f6729a16134
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2018-07-25 09:27:14 +0300

    spaces

--- src/app/sharing-variables/sharing-variables/sharing-variables.component.ts
+++ src/app/sharing-variables/sharing-variables/sharing-variables.component.ts
@@ -9,13 +9,13 @@ import { Title } from '@angular/platform-browser';
 export class SharingVariablesComponent implements OnInit {
 	constructor(private router: Router, private titleService: Title) {}
 	
-	@HostListener('document:keydown', ['$event'])								//when key is pressed
+	@HostListener('document:keydown', ['$event'])						//when key is pressed
 	handleKeyboardEvent(event: KeyboardEvent) {
-		if (event.key == 'ArrowRight') {										//if right arrow is pressed
-			this.nextPage();													//go to next page
+		if (event.key == 'ArrowRight') {						//if right arrow is pressed
+			this.nextPage();							//go to next page
 		}
-		else if (event.key == 'ArrowLeft') {									//if left arrow is pressed
-			this.previousPage();												//go to previous page
+		else if (event.key == 'ArrowLeft') {						//if left arrow is pressed
+			this.previousPage();							//go to previous page
 		}
 	}
 	
@@ -24,10 +24,10 @@ export class SharingVariablesComponent implements OnInit {
 	}
 	
 	previousPage() {
-		this.router.navigate(['/introduction']);								//navigate to
+		this.router.navigate(['/introduction']);					//navigate to
 	}
 
 	nextPage() {
-		this.router.navigate(['/web-storage']);									//navigate to
+		this.router.navigate(['/web-storage']);						//navigate to
 	}
 }
