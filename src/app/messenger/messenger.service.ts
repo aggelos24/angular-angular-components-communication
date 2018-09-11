f61013e26e627857ad9e90b7ffdcc566a7d36788
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2018-09-11 13:41:44 +0300

    naming

--- src/app/messenger/messenger.service.ts
+++ src/app/messenger/messenger.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
-import { Message } from '../interfaces/Message';
+import { Message } from '../interfaces/message';
 
 
 @Injectable()
